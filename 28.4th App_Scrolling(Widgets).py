80e6cef31252e722a728bdf296c0db673e715d14
AhnafTouseef <158799743+AhnafTouseef@users.noreply.github.com>
2025-01-15 21:31:19 +0600

    Update 28.4th App_Scrolling(Widgets).py

--- 28.4th App_Scrolling(Widgets).py
+++ 28.4th App_Scrolling(Widgets).py
@@ -9,22 +9,44 @@ class ListFrame(ttk.Frame):
         # Widget data
         self.text_data = text_data
         self.item_number = len(text_data)
-        self.item_height = self.item_number * item_height
+        self.list_height = self.item_number * item_height
 
         # canvas
-        self.canvas = tk.Canvas(self,background='red', scrollregion=(0,0,500,self.item_height))
+        self.canvas = tk.Canvas(self,background='red', scrollregion=(0,0,self.winfo_width(),self.list_height))
         self.canvas.pack(fill='both', expand=True)
 
         # Display frame
         self.frame = ttk.Frame(self)
         
         for index, item in enumerate(self.text_data):
-            self.create_item(index, item).pack(expand=True, fill='both', padx=5, pady=5)
+            self.create_item(index, item).pack(expand=True, fill='both', padx=4, pady=10)
 
-        self.canvas.create_window((0,0), window=self.frame, anchor='nw', width=500, height=self.item_height)
-
+        # Scrollbar
+        self.scrollbar = ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+        
         # event 
-        self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta/120), 'units'))
+        self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta/60), 'units'))
+        self.bind('<Configure>', self.update_size)
+
+    def update_size(self, event):
+        if self.list_height >= self.winfo_height():
+            height = self.list_height
+            self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta/60), 'units'))
+            self.scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
+
+
+        else:
+            height = self.winfo_height()
+            self.canvas.unbind_all('<MouseWheel>')
+            self.scrollbar.place_forget()
+
+        self.canvas.create_window((0,0),
+                                   window=self.frame, 
+                                   anchor='nw', 
+                                   width=self.winfo_width(), 
+                                   height=height)
 
     def create_item(self, index, item):
         frame = ttk.Frame(self.frame)
@@ -34,8 +56,8 @@ class ListFrame(ttk.Frame):
         frame.columnconfigure((0,1,2,3,4), weight=1, uniform='a')
 
         # Widgets
-        ttk.Label(frame, text = f'#{index}').grid(row=0, column=0, sticky='nsew')
-        ttk.Label(frame, text = item[0]).grid(row=0, column=1, sticky='nsew')
+        ttk.Label(frame, text = f'#{index}').grid(row=0, column=0)
+        ttk.Label(frame, text = item[0]).grid(row=0, column=1)
         ttk.Button(frame, text = item[1]).grid(row=0, column=2, columnspan=3, sticky='nsew')
 
         return frame
